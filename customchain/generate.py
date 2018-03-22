b0fac9bed372af3e241cacc160e788c37f623e69
Juan Antonio López Mendoza <juanlopezm94@gmail.com>
2018-03-22 17:53:47 +0100

    Fix wrong argument definition in function

--- customchain/generate.py
+++ customchain/generate.py
@@ -14,7 +14,7 @@ def generate_docker_compose_header():
     f.write("version: '2.0'\nservices:\n")
     f.close()
 
-def generate_docker_compose(name):
+def generate_docker_compose(hosts):
     f = open(docker_template,"r")
     spec = f.read()
     f.close()
