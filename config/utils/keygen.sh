666be7b3767114ab4671e1c2b32e37cc8e3d886b
David Dorgan <david@parity.io>
2018-12-12 20:31:33 +0000

    Use downloaded ethkey version

--- config/utils/keygen.sh
+++ config/utils/keygen.sh
@@ -11,7 +11,7 @@ fi
 
 
 # Generate the private and public keys
-ethkey generate random > $TMPFILE
+./ethkey generate random > $TMPFILE
  
 cat $TMPFILE | grep public | awk {'print $2'} > $1/key.pub
 cat $TMPFILE | grep secret | awk {'print $2'} > $1/key.priv
